dab76da5586b30a5a7fb3204e800063c6690dca0
Syrows <syrows.development@gmail.com>
2020-06-26 19:10:09 +0200

    Changing test file.

--- src/main/java/fr/syrows/Test.java
+++ src/main/java/fr/syrows/Test.java
@@ -1,7 +1,5 @@
 package fr.syrows;
 
-import fr.syrows.inventories.interfaces.EasyInventory;
-import fr.syrows.inventories.interfaces.impl.FastInventory;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,28 +13,9 @@ public class Test implements Listener {
         this.plugin = plugin;
     }
 
-    public EasyInventory getInventory() {
-
-        FastInventory inventory = new FastInventory.Builder(this.plugin.getDefaultManager())
-                .withIdentifier("my_inventory")
-                .withTitle("&eInventory")
-                .withSize(54)
-                .get();
-
-        this.setContents(inventory);
-
-        return inventory;
-    }
-
-    public void setContents(EasyInventory inventory) {
-
-    }
-
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
 
         if(event.getItem() == null || event.getItem().getType() != Material.STONE) return;
-
-        this.getInventory().open(event.getPlayer());
     }
 }
